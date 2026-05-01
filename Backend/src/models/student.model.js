60f425cb5b789d406a6f1b063472878df80820dc
Rushi23442 <waghrushi228@gmail.com>
2026-05-01 18:39:11 +0530

    added all models

---
+++ Backend/src/models/student.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    rollNumber: { 
+        type: String,
+        required: true,
+        unique: true
+    },
+    class: {
+        type: String,
+        required: true  
+    },
+    division: {
+        type: String,
+    },
+    subjects: [{
+        type: String,
+    }],
+    batch: {
+        type: String,
+    },
+    finalVerification: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Verification"
+    },
+    Submission:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Submission"
+    },
+    isFinalSubmitted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+const Student = mongoose.model("Student", studentSchema);
+
+export default Student;
---
+++ Backend/src/models/submission.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose"
+
+const submissionSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },  
+    subjectType: {
+        type: String,
+        enum: ["Theory", "Practical"],
+        required: true
+    },
+    class:{
+        type: String,
+        required: true
+    },
+    division:{
+        type: String,
+        required: true
+    },
+    batch:{
+        type: String,
+    },
+    teacherId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    status:{
+        type: String,
+        enum: ["Completed", "Not Completed"],
+        default: "Not Completed"
+    },
+    markedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
+
+const Submission = mongoose.model("Submission", submissionSchema);
+
+export default Submission;
