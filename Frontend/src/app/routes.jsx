7cb5dc5540f7e0b1ff9216b2500ac7055ed25797
Ketan <dewalkarketan123@gmail.com>
2026-05-06 00:03:20 +0530

    implemented the routes structure

--- Frontend/src/app/routes.jsx
+++ Frontend/src/app/routes.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Navbar from "../components/ui/Navbar";
 import HeroSection from "../pages/Home/LandingPage";
 import Login from "../pages/Auth/Login";
+import { useAuthStore } from "./stores";
+
+const user = useAuthStore.getState().user;
 
 const PublicRoutes = [
   {
@@ -19,6 +22,39 @@ const PublicRoutes = [
     element: <Login />,
   },
 ];
-const PrivateRoutes = [];
 
-export const router = createBrowserRouter([...PublicRoutes, ...PrivateRoutes]);
+const StudentRoutes = [
+  {
+    path: "/student",
+    element: <div>Student Dashboard</div>,
+  },
+];
+
+const HODRoutes = [
+  {
+    path: "/hod",
+    element: <div>HOD Dashboard</div>,
+  },
+];
+
+const ClassCoordinatorRoutes = [
+  {
+    path: "/classcoordinator",
+    element: <div>Class Coordinator Dashboard</div>,
+  },
+];
+
+const FacultyTeacherRoutes = [
+  {
+    path: "/teacher",
+    element: <div>Faculty Teacher Dashboard</div>,
+  },
+];
+
+export const router = createBrowserRouter([
+  ...PublicRoutes,
+  ...(user?.role === "Student" ? StudentRoutes : []),
+  ...(user?.role === "HOD" ? HODRoutes : []),
+  ...(user?.role === "ClassCoordinator" ? ClassCoordinatorRoutes : []),
+  ...(user?.role === "Teacher" ? FacultyTeacherRoutes : []),
+]);
